52746ee8dfec0d7e97aebcffe45a12f2a982e3b4
Matt Hayes <matt@mysterycommand.com>
2017-04-16 18:50:53 -0400

    refactor: start with a simple map and mesh renderer, but the vertices are backwards or something

--- Assets/Scripts/BearshirtLoop.cs
+++ Assets/Scripts/BearshirtLoop.cs
@@ -1,16 +1,55 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace Bearshirt
 {
 	public class BearshirtLoop : MonoBehaviour
 	{
-		/// <summary>
-		/// Start is called on the frame when a script is enabled just before
-		/// any of the Update methods is called the first time.
-		/// </summary>
 		void Start()
 		{
 			Debug.Log("BearshirtLoop");
+			BearshirtMap map = new BearshirtMap(2, 2);
+
+			MeshFilter filter = GetComponent<MeshFilter>();
+			Mesh mesh = new Mesh();
+
+			float size = 1f,
+				left = -(map.width * size) / 2,
+				top = -(map.height * size) / 2;
+
+			List<Vector3> verts = new List<Vector3>();
+			List<int> tris = new List<int>();
+
+			map.ForEach((int x, int y) => {
+				float t = top + y * size;
+				float r = left + (x + 1) * size;
+				float b = top + (y + 1) * size;
+				float l = left + x * size;
+
+				Vector3 lt = new Vector3(l, t, 0f);
+				Vector3 rt = new Vector3(r, t, 0f);
+				Vector3 rb = new Vector3(r, b, 0f);
+				Vector3 lb = new Vector3(l, b, 0f);
+
+				if (!verts.Contains(lt)) verts.Add(lt);
+				if (!verts.Contains(rt)) verts.Add(rt);
+				if (!verts.Contains(rb)) verts.Add(rb);
+				if (!verts.Contains(lb)) verts.Add(lb);
+
+				tris.Add(verts.IndexOf(lt));
+				tris.Add(verts.IndexOf(rt));
+				tris.Add(verts.IndexOf(rb));
+
+				tris.Add(verts.IndexOf(lt));
+				tris.Add(verts.IndexOf(rb));
+				tris.Add(verts.IndexOf(lb));
+			});
+
+			mesh.vertices = verts.ToArray();
+			mesh.triangles = tris.ToArray();
+			mesh.RecalculateNormals();
+
+			filter.mesh = mesh;
 		}
 	}
 }
--- Assets/Scripts/BearshirtMap.cs
+++ Assets/Scripts/BearshirtMap.cs
@@ -1,21 +1,66 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
 namespace Bearshirt
 {
 	public class BearshirtMap
 	{
-		private List<int> seeds = new List<int>();
+		public int width { get; private set; }
+		public int height { get; private set; }
+		private int seed;
 		private System.Random rando;
 
-		BearshirtMap(int? seed)
+		private Texture2D map;
+
+		public BearshirtMap(int _width, int _height)
 		{
-			seeds.Add(GetSeed(seed));
+			width = _width;
+			height = _height;
+
+			seed = GetSeed(null);
+			rando = new System.Random(seed);
+			Debug.Log("BearshirtMap: " + seed);
+
+			map = GetTexture(width, height);
+		}
+
+		private Texture2D GetTexture(int width, int height)
+		{
+			Texture2D tex = new Texture2D(width, height);
+			ForEach((int x, int y) => {
+				float v = rando.Next(0, 1);
+				tex.SetPixel(x, y, new Color(v, v, v));
+			});
+			tex.Apply();
+			return tex;
 		}
 
 		private int GetSeed(int? seed)
 		{
 			return seed ?? Time.time.ToString().GetHashCode();
+		}
+
+		public void ForRange(
+			int fx = 0, int tx = 0, int fy = 0, int ty = 0,
+			Action<int, int> action = null)
+		{
+			if (action == null)
+			{
+				action = (int x, int y) => {};
+			}
+
+			for (int x = fx; x < tx; ++x)
+			{
+				for (int y = fy; y < ty; ++y)
+				{
+					action(x, y);
+				}
+			}
+		}
+
+		public void ForEach(Action<int, int> action = null)
+		{
+			ForRange(0, width, 0, height, action);
 		}
 	}
 }
