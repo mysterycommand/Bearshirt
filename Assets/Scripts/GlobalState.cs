d2960d33f5bc69cf67af43bf4767aa226553e12d
Matt Hayes <matt@mysterycommand.com>
2017-04-19 22:46:52 -0400

    feat: track levels and deaths

--- Assets/Scripts/GlobalState.cs
+++ Assets/Scripts/GlobalState.cs
@@ -2,7 +2,36 @@
 {
 	public class GlobalState
 	{
-		public static bool IsAtDoor = false;
-		public static bool IsInLava = false;
+		private static bool _IsAtDoor = false;
+		public static int Levels { get; private set; } = 0;
+
+		private static bool _IsInLava = false;
+		public static int Deaths { get; private set; } = 0;
+
+		public static bool IsAtDoor
+		{
+			get { return _IsAtDoor; }
+			set
+			{
+				_IsAtDoor = value;
+				if (_IsAtDoor)
+				{
+					++Levels;
+				}
+			}
+		}
+
+		public static bool IsInLava
+		{
+			get { return _IsInLava; }
+			set
+			{
+				_IsInLava = value;
+				if (_IsInLava)
+				{
+					++Deaths;
+				}
+			}
+		}
 	}
 }
